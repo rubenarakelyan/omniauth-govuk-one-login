a875d09fb4dd6c9b6ba6155c496e9aa44dad72e6
Ruben Arakelyan <ruben@arakelyan.uk>
2025-06-25 09:54:07 +0100

    Fix rubocop error

--- lib/omniauth/govuk_one_login/secure_compare.rb
+++ lib/omniauth/govuk_one_login/secure_compare.rb
@@ -3,6 +3,7 @@ module OmniAuth
     class SecureCompare
       # Borrowed from Devise.secure_compare
       # Ref: https://github.com/heartcombo/devise/blob/cf93de390a29654620fdf7ac07b4794eb95171d0/lib/devise.rb#L520-L527
+      # rubocop:disable Naming/PredicateMethod
       def self.secure_compare(a, b)
         return false if a.blank? || b.blank? || a.bytesize != b.bytesize
 
@@ -12,6 +13,7 @@ module OmniAuth
         b.each_byte { |byte| res |= byte ^ l.shift }
         res.zero?
       end
+      # rubocop:enable Naming/PredicateMethod
     end
   end
 end
